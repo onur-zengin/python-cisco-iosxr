ae94b4223348c2960497a1a98ab3bed4c6b0888f
onur-zengin <zengin.onur@gmail.com>
2016-12-28 00:24:47 +0000

    completed main func

--- router.py
+++ router.py
@@ -7,17 +7,32 @@ class Router():
     def __init__(self, ipadd):
         self.ipadd = ipadd
 
+def usage(args):
+    print 'USAGE:\n\t%s [-i <filename>] [--input <filename>]' % (args[0])
+
 def main(args):
-    inputfile = 'abc'
-    print getopt.getopt(args, "hi:", "inputfile")
+    try:
+        options, remainder = getopt.getopt(args, "i:", ["input="])
+    except getopt.GetoptError as err:
+        print err
+        usage(sys.argv)
+        exit()
+    for opt, arg in options:
+        if opt in ('-i','--input'):
+            inputfile = arg
+        else:
+            assert False, "unhandled option"
     try:
         with open(inputfile) as sf:
             nodes = [n.strip('\n') for n in sf.readlines()]
-        print nodes
+            print nodes
     except IOError:
         print 'Input file (%s) could not be located.' % (inputfile)
         exit()
 
-
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
+    else:
+        usage(sys.argv)
+        exit() #sys.exit(2)
