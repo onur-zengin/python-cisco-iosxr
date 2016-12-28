4ac8a27ab6d279a26716bb015210e22b0367cc70
onur-zengin <zengin.onur@gmail.com>
2016-12-28 13:37:45 +0000

    main function completed

--- router.py
+++ router.py
@@ -2,10 +2,21 @@
 
 import sys
 import getopt
+import csv
+import socket
 
-class Router():
-    def __init__(self, ipadd):
-        self.ipadd = ipadd
+class Router(object):
+    def __init__(self, name):
+        self.name = name
+        #self.ipaddr = socket.gethostbyname(name)
+
+def parser(lst):
+    dict = {}
+    for node in [line.split(':') for line in lst]:
+        dict[node[0]] = {}
+        for i in range(len(node))[1:]:
+            dict[node[0]][node[i].split(',')[0]] = [int for int in node[i].split(',')[1:]]
+    return dict
 
 def usage(args):
     print 'USAGE:\n\t%s [-i <filename>] [--input <filename>]' % (args[0])
@@ -16,7 +27,7 @@ def main(args):
     except getopt.GetoptError as err:
         print err
         usage(sys.argv)
-        exit()
+        sys.exit(2)
     for opt, arg in options:
         if opt in ('-i','--input'):
             inputfile = arg
@@ -24,15 +35,19 @@ def main(args):
             assert False, "unhandled option"
     try:
         with open(inputfile) as sf:
-            nodes = [n.strip('\n') for n in sf.readlines()]
-            print nodes
+            inventory = parser([n.strip('\n') for n in sf.readlines()])
     except IOError:
         print 'Input file (%s) could not be located.' % (inputfile)
-        exit()
+        sys.exit(1)
+    print "continue working on inventory "
+    print inventory
+    for node in inventory.keys():
+        a = Router(node)
+        print a.name
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         main(sys.argv[1:])
     else:
         usage(sys.argv)
-        exit() #sys.exit(2)
+        sys.exit(2)
