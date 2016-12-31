bcfc93ba0a0d912eec912f279a80112b4d82e778
onur-zengin <zengin.onur@gmail.com>
2016-12-31 23:34:13 +0000

    Saturday's work

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # laphroaig
+TODO
+- Check memory util after long loop
+- Check single process not completing in predefined frequency (ping test)
+
--- router.py
+++ router.py
@@ -7,9 +7,7 @@ import threading
 import logging
 import time
 import subprocess
-
-asctime = time.asctime()
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s [%(levelname)s] %(threadName)-10s: %(message)s') # FIXME revisit formatting %-Ns
+import re
 
 oidw = [
 	'IfName:','1.3.6.1.2.1.31.1.1.1.1', # :IfName
@@ -22,24 +20,44 @@ class Router(threading.Thread):
         self.node = node
         self.interfaces = interfaces
     def run(self):
-        logging.debug("starting")
+        logging.info("Starting")
         self.ipaddr = ''
-        print self.node
-        print self.interfaces
         try:
             self.ipaddr = socket.gethostbyname(self.node)
-        except socket.gaierror:
-            print "Hostname not found"
-            logging.debug("exiting early")
-            sys.exit(3)
-        print self.ipaddr
-        self.ping(self.ipaddr)
-        self.snmpwalk(self.ipaddr, self.oid)
-        logging.debug("exiting")
+        except socket.gaierror as gaierr:
+            logging.warning("Operation halted: %s" % (str(gaierr)))
+            sys.exit(3) #wait for the event
+        except:
+            logging.debug("Unexpected error while resolving hostname: %s" % (str(sys.exc_info()[:2])))
+            #raise
+            sys.exit(3) #wait for the event
+        #print "I'm still alive!"
+        ping = self.ping(self.ipaddr)
+        if ping[0] == 0:
+            self.snmpwalk(self.ipaddr, self.oid)
+        else:
+            logging.debug("Unexpected error during ping test: ### %s ###" % (str(ping[1])))
+            sys.exit(3)  # wait for the event
+        logging.info("Completed")
     def ping(self,ipaddr):
-        itup = subprocess.Popen(['ping', '-i', '0.2', '-w', '2', '-c', '5', ipaddr, '-q'], stdout=subprocess.PIPE,
+        pingr = 1
+        ptup = None
+        try:
+            ptup = subprocess.Popen(['ping', '-i', '0.2', '-w', '2', '-c', '5', ipaddr, '-q'], stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE).communicate()
-        print itup
+        except:
+            logging.debug("Unexpected error - Popen function (ping): %s" % (str(sys.exc_info()[:2])))
+        else:
+            if ptup[1] == '':
+                n = re.search(r'(\d+)\%\spacket loss', ptup[0])
+                if n != None:
+                    if int(n.group(1)) == 0:
+                        pingr = 0
+                    elif 0 < int(n.group(1)) < 100:
+                        logging.warning("Operation halted. Packet loss detected")
+                    elif int(n.group(1)) == 100:
+                        logging.warning("Operation halted. Node unreachable")
+        return pingr, ptup
     def snmpwalk(self,ipaddr,oid):
         stup = subprocess.Popen(['snmpwalk', '-Oqv', '-v2c', '-c', 'kN8qpTxH', ipaddr, oid], stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE).communicate()
@@ -54,11 +72,23 @@ def parser(lst):
     return dict
 
 def usage(args):
-    print 'USAGE:\n\t%s [-i <filename>] [--input <filename>]' % (args[0])
+    print 'USAGE:\n\t%s [-i <filename>] [--input <filename>] [-l <loglevel>] [--logging <loglevel>]' % (args[0])
+    print 'DESCRIPTION:\n\t[-i <filename>], [--input <filename>]' \
+          '\n\t\tThe inventory details must be provided in a text file structured in the following format,' \
+          'while each node being written on a separate line:' \
+          '\n\t\t\t<nodename>:pni,<intname-1>,...,<intname-M>:cdn,<intname-1>,...,<intname-N>' \
+          '\n\t\t\tEXAMPLE: er12.thlon:pni,Bundle-Ether1024,Bundle-Ether1040:cdn,Bundle-Ether1064' \
+          '\n\t[-l <loglevel>], [--logging <loglevel>]' \
+          '\n\t\tThe loglevel must be specified as one of INFO, WARNING, DEBUG in capital letters. ' \
+          'If none specified, the program will run with default level INFO.'
 
 def main(args):
+    asctime = time.asctime()
+    loglevel = 'INFO'
+    runtime = 'infinite'
+    frequency = 5
     try:
-        options, remainder = getopt.getopt(args, "i:", ["input="])
+        options, remainder = getopt.getopt(args, "i:l:r:f:", ["input=", "logging=", "repeat=", "frequency="])
     except getopt.GetoptError as err:
         print err
         usage(sys.argv)
@@ -66,22 +96,40 @@ def main(args):
     for opt, arg in options:
         if opt in ('-i','--input'):
             inputfile = arg
+        elif opt in ('-l','--logging'):
+            if arg in ('INFO','WARNING','DEBUG'):
+                loglevel = arg
+            else:
+                loglevel = 'INFO'
+        elif opt in ('-r','--repeat'):
+            runtime = int(arg)
+        elif opt in ('-f','--frequency'):
+            frequency = int(arg)
         else:
             assert False, "unhandled option"
-    try:
-        with open(inputfile) as sf:
-            inventory = parser([n.strip('\n') for n in sf.readlines()])
-    except IOError:
-        print 'Input file (%s) could not be located.' % (inputfile)
-        sys.exit(1)
-    threads = []
-    logging.debug("Initializing SubThreads")
-    for n,node in enumerate(inventory):
-        t = Router(n+1,node,inventory[node])
-        #threads.append(t)
-        t.start()
-        time.sleep(1)
-    #print threads
+    logging.basicConfig(level=logging.getLevelName(loglevel),
+                        format='%(asctime)-15s [%(levelname)s] %(threadName)-10s: %(message)s')  # FIXME revisit formatting %-Ns
+    while True:
+        try:
+            with open(inputfile) as sf:
+                inventory = parser([n.strip('\n') for n in sf.readlines()])
+        except IOError:
+            print 'Input file (%s) could not be located.' % (inputfile)
+            sys.exit(1)
+        else:
+            threads = []
+            logging.debug("Initializing subThreads")
+            for n,node in enumerate(inventory):
+                t = Router(n+1,node,inventory[node])
+                #threads.append(t)
+                t.start()
+                time.sleep(1)
+            if type(runtime) == int:
+                runtime -= 1
+        finally:
+            if runtime == 0:
+                break
+            time.sleep(frequency)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
