daf610f644328bbdcc906fd6fffb9772faf60233
onur-zengin <zengin.onur@gmail.com>
2017-01-03 21:33:01 +0000

    Working on discovery()

--- pniMonitor.py
+++ pniMonitor.py
@@ -36,8 +36,9 @@ class Router(threading.Thread):
             iflist, iplist = self.discovery(self.ipaddr, self.oids)
             for interface in self.interfaces:
                 for i in iflist:
-                    if interface in i:
-                        print i
+                    print i.split('')
+                    #if interface in i:
+                     #   print i
         #self.snmpwalk(self.ipaddr, self.oid)
         logging.info("Completed")
     def dns(self,node):
