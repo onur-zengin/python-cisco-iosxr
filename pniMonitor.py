b13f69aafdcf6347b7e4fa114444b0bd445c0126
onur-zengin <zengin.onur@gmail.com>
2017-01-20 13:05:20 +0000

    updating discovery() with cbgpPeer2index

--- pniMonitor.py
+++ pniMonitor.py
@@ -45,7 +45,6 @@ class Router(threading.Thread):
         self.switch = dswitch
         self.risk_factor = risk_factor
         self.pni_identifier, self.cdn_identifier = int_identifiers
-        print self.pni_identifier
     def run(self):
         logging.debug("Starting")
         self.tstamp = tstamp('mr')
@@ -123,6 +122,8 @@ class Router(threading.Thread):
         disc = {}
         ifNameTable, ifDescrTable, ipTable, peerTable = tuple([i.split(' ') for i in n] for n in
                                             map(lambda oid: self.snmp(ipaddr, [oid], quiet='off'), self.dsc_oids))
+        for i in ifDescrTable:
+            print str(i[3:])
         for i, j in zip(ifDescrTable, ifNameTable):
             if 'no-mon' not in str(i[3:]) and self.pni_identifier in str(i[3:]) and 'Bundle-Ether' in j[3]:
                 pni_interfaces.append(j[3])
