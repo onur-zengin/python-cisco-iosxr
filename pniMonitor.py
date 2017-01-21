260164f728914dafe6a5c64b6d6842fe7f71787d
onur-zengin <zengin.onur@gmail.com>
2017-01-21 15:25:47 +0000

    updating probe() with bgp oids

--- pniMonitor.py
+++ pniMonitor.py
@@ -214,16 +214,17 @@ class Router(threading.Thread):
                 nxt[interface]['ifInOctets'] = int_status[3]
                 nxt[interface]['ifOutOctets'] = int_status[4]
                 if disc[interface]['type'] == 'pni':
+                    nxt[interface]['peerStatus'] = {}
                     if disc[interface].has_key('peer_ipv4'):
                         for n in disc[interface]['peer_ipv4']:
                             peer_status = self.snmp(ipaddr, [self.bgp_oids[0] + '.' + n[1]]
                                                     + [self.bgp_oids[1] + '.' + n[1] + '.1.1'], cmd='snmpget')
-                            nxt[interface]['peerStatus'] = {n[0]:peer_status}
+                            nxt[interface]['peerStatus'][n[0]] = peer_status
                     if disc[interface].has_key('peer_ipv6'):
                         for n in disc[interface]['peer_ipv6']:
                             peer_status = self.snmp(ipaddr, [self.bgp_oids[0] + '.' + n[1]]
                                                     + [self.bgp_oids[1] + '.' + n[1] + '.2.1'], cmd='snmpget')
-                            nxt[interface]['peerStatus'] = {n[0]: peer_status}
+                            nxt[interface]['peerStatus'][n[0]] = peer_status
                     if not disc[interface].has_key('peer_ipv4') and not disc[interface].has_key('peer_ipv6'):
                         nxt[interface]['peerStatus'] = None
                         logging.warning("PNI interface %s has no BGP sessions" % interface)
