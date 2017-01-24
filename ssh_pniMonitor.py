c675861fc1c625ff1ce50f78955e50b5ab85bb54
onur-zengin <zengin.onur@gmail.com>
2017-01-24 10:26:17 +0000

    working on ssh_pniMon.py

--- ssh_pniMonitor.py
+++ ssh_pniMonitor.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
 
 import paramiko
+import getpass
+
+un = getpass.getuser()
+print un
 
 node = 'er10.bllab'
 
+"""
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(node, username='onur.zengin', password='Qtbhm55t!')
+ssh.connect(node, un, pw)
 
 stdin, stdout, stderr = ssh.exec_command("sh version")
 type(stdin)
 
 a = stdout.readlines()
 print a
+"""
