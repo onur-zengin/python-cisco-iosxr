7f25a9f1fbad56772db7a1e793d2ed7f9eba810e
onur-zengin <zengin.onur@gmail.com>
2017-01-24 10:46:34 +0000

    working on ssh_pniMon.py

--- ssh_pniMonitor.py
+++ ssh_pniMonitor.py
@@ -7,7 +7,7 @@ import os
 hd = os.environ['HOME']
 un = getpass.getuser()
 try:
-    pw = getpass.getpass('Enter the cauth password for user %s:' % un, stream=None)
+    pw = getpass.getpass('Enter cauth password for user %s:' % un, stream=None)
 except getpass.GetPassWarning as pw_warning:
     print pw_warning
     raise
@@ -24,4 +24,4 @@ type(stdin)
 
 a = stdout.readlines()
 for i in a:
-    print i
+    print i.strip('\n')
