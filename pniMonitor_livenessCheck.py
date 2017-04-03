b5f663de7713aa4e091d8615aed3a63c026584f1
onur-zengin <zengin.onur@gmail.com>
2017-04-03 17:56:07 +0100

    Working on liveness check

--- pniMonitor_livenessCheck.py
+++ pniMonitor_livenessCheck.py
@@ -1,92 +1,94 @@
 #!/usr/bin/env python
 
+import sys
 import logging
 import logging.handlers
-import sys
-import re
+import argparse
 import getopt
+import os
+import os.path
+import re
 
-FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
-logging.basicConfig(format=FORMAT)
-d = {'clientip': '192.168.0.1', 'user': 'fbloggs'}
-logger = logging.getLogger(__main__)
-logger.warning('Protocol problem: %s', 'connection reset', extra=d)
+email_distro = ['onur.zengin@sky.uk']
 
+logFormatter = logging.Formatter('%(asctime)-15s [%(levelname)s]: %(message)s')
+rootLogger = logging.getLogger()
 
-format = logging.Formatter('%(asctime)-15s [%(levelname)s]: %(message)s')
-eh = logging.handlers.SMTPHandler('localhost', 'no-reply@automation.skycdp.com', email_distro,
-                                  'CRITICAL: PNI Monitor Liveness Check Failed')
-eh.setFormatter(format)
-eh.setLevel(logging.CRITICAL)
-logger.addHandler(eh)
+emailHandler = logging.handlers.SMTPHandler('localhost', 'no-reply@automation.skycdp.com', email_distro,
+                                            'CRITICAL: PNI Monitor Liveness Check Failed')
+emailHandler.setFormatter(logFormatter)
+emailHandler.setLevel(logging.CRITICAL)
+rootLogger.addHandler(emailHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
+
 
 def main(args):
-    email_distro = []
+    global email_distro
+    global emailHandler
     try:
         options, remainder = getopt.getopt(args[1:], "c:", ["config="])
     except getopt.GetoptError as getopterr:
-        print getopterr
+        rootLogger.error(getopterr)
         sys.exit(2)
     else:
         for opt, arg in options:
             if opt in ('-c', '--config'):
                 config_file = arg
             else:
-                print "Invalid option specified on the command line: %s" % opt
+                rootLogger.error("Invalid option specified on the command line: %s" % opt)
                 sys.exit(2)
     try:
         with open(config_file) as pf:
             parameters = [tuple(i.split('=')) for i in
                           filter(lambda line: line[0] != '#', [n.strip('\n')
                                                                for n in pf.readlines() if n != '\n'])]
-    except IOError as ioerr:
-        print ioerr
-        rg = re.search(r'(\'.+\')', str(ioerr))
-        print "%r" % rg.group(1)[3:]
-        #logging.CRITICAL("Liveness Check could not run. %r could not be located.", rg.group(1)[3:])
+    except IOError:
+        rootLogger.critical("Liveness Check Failed. %r could not be located.", config_file)
     else:
         try:
             for opt, arg in parameters:
-                if opt == 'email_alert_severity':
-                    if arg.lower() in ('warning', 'error', 'critical'):
-                        if email_alert_severity != arg.upper():
-                            logging.info('Email alert severity has been updated: %s' % arg.upper())
-                        email_alert_severity = arg.upper()
-                    else:
-                        logging.warning('Invalid severity specified for email alerts. Resetting to last '
-                                                'known good configuration: %s' % email_alert_severity)
-
-                elif opt == 'email_distribution_list':
+                if opt == 'email_distribution_list':
                     split_lst = arg.split(',')
                     try:
                         for email in split_lst:
                             match = re.search(r"[\w.-]+@(sky.uk|bskyb.com)", email)
                             match.group()
                     except AttributeError:
-                        logging.warning('Invalid email address found in the distribution list. Resetting '
-                                                'to last known good configuration: %s' % email_distro)
+                        rootLogger.warning('Invalid email address found in the distribution list. Resetting to default '
+                                           'configuration: %s' % email_distro)
                     else:
                         if email_distro != split_lst:
-                            logging.info('Email distribution list has been updated: %s' % split_lst)
+                            rootLogger.info('Email distribution list has been updated: %s' % split_lst)
                         email_distro = split_lst
-                elif opt.lower() in ('pni_interface_tag', 'cdn_interface_tag', 'ssh_loglevel',
-                                     'acl_name', 'persistence', 'inventory_file', 'ssh_timeout'):
-                    pass
                 else:
-                   logging.warning("Invalid parameter found in the configuration file: (%s). The program "
-                                            "will continue with the last known good configuration. Use '%s -m' or '%s "
-                                            "--manual' to see detailed usage instructions." % (opt, args[0], args[0]))
+                    pass
         except ValueError:
             logging.warning("Invalid configuration line detected and ignored. All configuration parameters must "
-                                "be provided as key value pairs separated by an equal sign (=). Use '%s -m' or '%s "
-                                "--manual' for more details." % (args[0], args[0]))
+                            "be provided as key value pairs separated by an equal sign (=).")
     finally:
-        #logging.debug("\tFrequency: %s\n\tRisk Factor: %s\n\tCDN Serving Cap: %s\n\tIPv4 Min Prefixes: %s"
-         #                 "\n\tIPv6 Min Prefixes: %s\n\tLog Level: %s\n\tLog Retention: %s\n\tEmail Alert Sev: %s"
-          #                "\n\tSimulation Mode: %s\n\tRuntime: %s\n\tEmail distribution list: %s"
-           #               % (frequency, risk_factor, cdn_serving_cap, ipv4_min_prefixes, ipv6_min_prefixes,
-            #                 log_retention, email_alert_severity, runtime, email_distro))
-
+        try:
+            rootLogger.removeHandler(emailHandler)
+        except NameError:
+            pass
+        emailHandler = logging.handlers.SMTPHandler('localhost', 'no-reply@automation.skycdp.com', email_distro,
+                                                    'CRITICAL: PNI Monitor Liveness Check Failed')
+        emailHandler.setFormatter(logFormatter)
+        emailHandler.setLevel(logging.CRITICAL)
+        rootLogger.addHandler(emailHandler)
+    pid_file = config_file.split(".")[0] + '.pid'
+    try:
+        with open(pid_file) as pf:
+            pid = pf.read().strip("'")
+    except IOError:
+        rootLogger.critical("Liveness Check Failed. %r could not be located.", pid_file)
+    except:
+        rootLogger.critical("Liveness Check Failed. Unexpected error: %r %r", sys.exc_info()[0], sys.exc_info()[1])
+    else:
+        if not os.path.exists("/proc/" + pid):
+            rootLogger.critical("Liveness Check Failed. No process found with PID#%r.", pid)
 
 if __name__ == '__main__':
     main(sys.argv)
