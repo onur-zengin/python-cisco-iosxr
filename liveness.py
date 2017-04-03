949a8fa59aeaabbdbaaab9c378f3abc2b37fd23c
onur-zengin <zengin.onur@gmail.com>
2017-04-03 12:06:28 +0100

    writing program pid (str) to the .pid file during startup

--- liveness.py
+++ liveness.py
@@ -6,6 +6,7 @@ import logging.handlers
 import argparse
 import getopt
 import os
+import fcntl
 
 def main(args):
     loglevel = 'CRITICAL'
@@ -13,6 +14,11 @@ def main(args):
     fp = open(args[0][:-3] + ".pid", 'w')
     fp.write(str(os.getpid()))
     print os.getpid()
+    try:
+        fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except IOError:
+        print "Another instance is already running."
+        sys.exit(1)
     #fp.read()
 
 
