720c0d7878939680aa438066f9e86475d8956324
onur-zengin <zengin.onur@gmail.com>
2017-04-03 11:52:35 +0100

    writing program pid (str) to the .pid file during startup

--- liveness.py
+++ liveness.py
@@ -5,10 +5,16 @@ import logging
 import logging.handlers
 import argparse
 import getopt
+import os
 
 def main(args):
     loglevel = 'CRITICAL'
     email_distro = []
+    fp = open(args[0][:-3] + ".pid", 'w')
+    fp.write(str(os.getpid()))
+    print os.getpid()
+    #fp.read()
+
 
 if __name__ == '__main__':
     main(sys.argv)
